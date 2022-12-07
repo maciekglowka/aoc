3379f26bf1e23c8b35c7b3abe3ad97e58f513e1e
maciek.glowka <maciek.glowka@gmail.com>
2022-12-07 23:48:29 +0100

    007

--- src/007.rs
+++ src/007.rs
@@ -1,71 +1,145 @@
 use std::{
+    cell::RefCell,
     collections::HashMap,
-    fs
+    fs,
+    rc::{Rc, Weak}
 };
 
 #[derive(Debug)]
-enum Entity {
-    Dir(HashMap<String, Entity>),
-    File(u32)
+struct Node {
+    pub parent: RefCell<Weak<Node>>,
+    pub children: RefCell<HashMap<String, Rc<Node>>>,
+    pub size: u32
 }
 
-impl Entity {
-    pub fn insert(&mut self, name: String, entity: Entity) {
-        match self {
-            Entity::Dir(dir) => {
-                if !dir.contains_key(&name) { dir.insert(name, entity); }
-            },
-            _ => ()
-        };
+impl Node {
+    pub fn new(size: u32) -> Node {
+        Node {
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(HashMap::new()),
+            size
+        }
+    }
+    pub fn insert(&self, name: String, node: &Rc<Node>) {
+        self.children.borrow_mut().insert(name, Rc::clone(node));
+    }
+    pub fn get_parent(&self) -> Weak<Node> {
+        Weak::clone(&self.parent.borrow())
+    }
+    pub fn get_child(&self, name: &str) -> Weak<Node> {
+        Weak::clone(
+            &Rc::downgrade(self.children.borrow().get(name).unwrap())
+        )
     }
 }
 
-const INPUT_PATH: &str = "inputs/_007.txt";
+const INPUT_PATH: &str = "inputs/007.txt";
 
 fn main () {
     let file_str = fs::read_to_string(INPUT_PATH).unwrap();
     let lines: Vec<&str> = file_str.split('$').collect();
 
-    let mut root = Entity::Dir(HashMap::new());
-    let mut path: Vec<&str> = Vec::new();
-    // root.insert("a.txt".to_string(), Entity::File(8));
-    // root.insert("d".to_string(), Entity::Dir(HashMap::new()));
+    let mut root = Rc::new(Node::new(0));
+    let mut cur = Rc::clone(&root);
 
-    // println!("{:?}", root);
     for line in lines {
-        command(line, &mut path, &mut root);
+        cur = command(line, cur);
     }
+
+    let mut sizes = Vec::new();
+    let res = find_sizes_below(&root, sizes);
+    sizes = res.1;
+    let root_size = res.0;
+    
+    let free_size = 70000000 - root_size;
+    let needed_size = 30000000 - free_size;
+    println!("Low sum: {}", sizes.iter().sum::<u32>());
+
+    let mut above_sizes = Vec::new();
+    let above_res = find_sizes_above(&root, above_sizes, needed_size);
+    above_sizes = above_res.1;
+    above_sizes.sort();
+    println!("{:?}", above_sizes);
 }
 
-fn command<'a>(line: &'a str, path: &mut Vec<&'a str>, root: &'a mut Entity) {
-    if line.len() < 3 { return }
+fn command(line: &str, cur: Rc<Node>) -> Rc<Node> {
+    if line.len() < 3 { return cur }
     match &line[1..3] {
-        "cd" => cd(line, path),
-        "ls" => ls(line, path, root),
-        _ => return
-    }
+        "cd" => return cd(line, &cur),
+        "ls" => ls(line, &cur),
+        _ => ()
+    };
+    cur
 }
 
-fn cd<'a>(line: &'a str, path: &mut Vec<&'a str>) {
+fn cd(line: &str, cur: &Rc<Node>) -> Rc<Node> {
     let args: Vec<&str> = line.split_whitespace().collect();
-    if args.len() < 2 { return }
+    if args.len() < 2 { return Rc::clone(cur) }
 
     match args[1] {
-        "/" => path.clear(),
-        ".." => {path.pop(); },
-        a => path.push(a)
+        "/" => get_root(cur).upgrade().unwrap(),
+        ".." => cur.get_parent().upgrade().unwrap(),
+        a => cur.get_child(a).upgrade().unwrap()
     }
-
-    println!("CD {:?}", path);
 }
 
-fn ls<'a>(line: &'a str, path: &mut Vec<&'a str>, root: &'a mut Entity) {
+fn ls(line: &str, cur: &Rc<Node>) {
     let args: Vec<&str> = line.split('\n').collect();
     if args.len() < 2 { return }
-    let cur = traverse(path, root);
-    println!("LS {:?}", &args[1..])
+
+    for arg in &args[1..] {
+        let parts: Vec<&str> = arg.split_whitespace().collect();
+        if parts.len() < 2 { continue }
+        let size = match parts[0] {
+            "dir" => 0,
+            a => a.parse::<u32>().unwrap()
+        };
+        let n = Rc::new(Node::new(size));
+        *n.parent.borrow_mut() = Rc::downgrade(cur);
+        cur.insert(parts[1].to_string(), &n);
+    }
 }
 
-fn traverse<'a>(path: &mut Vec<&'a str>, root: &'a mut Entity) -> &'a mut Entity {
-    root
+fn get_root(node: &Rc<Node>) -> Weak<Node> {
+    let mut cur = Rc::clone(node);
+    loop {
+        cur = match cur.get_parent().upgrade() {
+            Some(n) => n,
+            None => return Rc::downgrade(&cur)
+        }
+    }
+}
+
+fn find_sizes_below(node: &Rc<Node>, mut sizes: Vec<u32>) -> (u32, Vec<u32>) {
+    let mut size = 0;
+    
+    for child in node.children.borrow().values() {
+        match child.size {
+            0 => {
+                let res = find_sizes_below(child, sizes);
+                size += res.0;
+                sizes = res.1;
+            },
+            a => size += a
+        }
+    }
+    if size < 100000 { sizes.push(size); }
+    (size, sizes)
+}
+
+fn find_sizes_above(node: &Rc<Node>, mut sizes: Vec<u32>, thresh: u32) -> (u32, Vec<u32>) {
+    let mut size = 0;
+    
+    for child in node.children.borrow().values() {
+        match child.size {
+            0 => {
+                let res = find_sizes_above(child, sizes, thresh);
+                size += res.0;
+                sizes = res.1;
+            },
+            a => size += a
+        }
+    }
+    if size >= thresh { sizes.push(size); }
+    (size, sizes)
 }
