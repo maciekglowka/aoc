e01e6fe0de0f9bb1087ae676e74eded8142b966c
maciek.glowka <maciek.glowka@gmail.com>
2022-12-17 08:30:46 +0100

    016 2nd

--- src/016.rs
+++ src/016.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     fs
 };
 
@@ -15,67 +15,107 @@ fn main () {
         })
         .collect();
 
-    let start = State {
-        positions: ["AA".to_string(), "AA".to_string()],
-        open_valves: HashSet::new(),
-        score_so_far: 0
-    };
+    let mut starting_pairs = HashSet::new();
 
-    let mut current_gen = vec!(start);
-
-    for step in 0..26 {
-        let mut next_gen = Vec::new();
-        for state in current_gen.iter() {
-            let new_score = get_current_score(
-                &valves,
-                &state.open_valves,
-                state.score_so_far
-            );
-
-            let mut actions: Vec<Vec<(String, bool)>> = state.positions.iter()
-                .map(|p| {
-                    get_possible_actions(
-                        &valves,
-                        &state.open_valves,
-                        p
-                    ).iter().map(|a| (a.clone(), a == p)).collect()
-                })
-                .collect();
-
-            let combinations: Vec<((String, bool),(String, bool))> = actions[0].iter()
-                .map(
-                    |a| actions[1].iter()
-                        .map(|b| (a.clone(), b.clone()))
-                        .collect::<Vec<((String, bool),(String, bool))>>()
-                )
-                .flatten()
-                .collect();
-
-            for combination in combinations.into_iter() {
-                let mut open = state.open_valves.clone();
-
-                if combination.0.1 { open.insert(combination.0.0.clone()); }
-                if combination.1.1 { open.insert(combination.1.0.clone()); }
-
-                let new_state = State {
-                    positions: [combination.0.0, combination.1.0],
-                    open_valves: open,
-                    score_so_far: new_score
-                };
-                next_gen.push(new_state);
-            }
+    for a in valves[&"AA".to_string()].connections.iter() {
+        for b in valves[&"AA".to_string()].connections.iter() {
+            if a == b { continue; }
+            if a <= b { starting_pairs.insert((a, b)); }
+            else { starting_pairs.insert((b, a));}
         }
-        next_gen.sort_by(|a, b| a.score_so_far.cmp(&b.score_so_far));
-        current_gen = next_gen.into_iter().rev().take(100000).collect();
-        println!("Step {}", step + 1);
+    }
+    // println!("{:?}", starting_pairs);
+    let mut total_scores = Vec::new();
+
+    for starting_pair in starting_pairs {
+
+
+        let start = State {
+            positions: [starting_pair.0.clone(), starting_pair.1.clone()],
+            open_valves: HashSet::new(),
+            score_so_far: 0
+        };
+
+        // println!("{}", dist(&valves, "JJ".to_string(), "HH".to_string()));
+
+        let mut current_gen = vec!(start);
+
+        for step in 0..26-1 {
+            let mut next_gen = Vec::new();
+            for state in current_gen.iter() {
+                let new_score = get_current_score(
+                    &valves,
+                    &state.open_valves,
+                    state.score_so_far
+                );
+
+                let mut actions: Vec<Vec<(String, bool)>> = state.positions.iter()
+                    .map(|p| {
+                        get_possible_actions(
+                            &valves,
+                            &state.open_valves,
+                            p
+                        ).iter().map(|a| (a.clone(), a == p)).collect()
+                    })
+                    .collect();
+
+                let combinations: Vec<((String, bool),(String, bool))> = actions[0].iter()
+                    .map(
+                        |a| actions[1].iter()
+                            .map(|b| (a.clone(), b.clone()))
+                            .collect::<Vec<((String, bool),(String, bool))>>()
+                    )
+                    .flatten()
+                    .collect();
+
+                for combination in combinations.into_iter() {
+                    let mut open = state.open_valves.clone();
+
+                    if combination.0.1 { 
+                        if valves[&combination.0.0].flow == 0 { continue; }
+                        else { open.insert(combination.0.0.clone()); }
+                    }
+                    if combination.1.1 { 
+                        if valves[&combination.1.0].flow == 0 { continue; }
+                        else {open.insert(combination.1.0.clone()); }
+                    }
+
+                    let new_state = State {
+                        positions: [combination.0.0, combination.1.0],
+                        open_valves: open,
+                        score_so_far: new_score
+                    };
+                    next_gen.push(new_state);
+                }
+            }
+            next_gen.sort_by(|a, b| {
+                let score_a = get_current_score(
+                    &valves,
+                    &a.open_valves,
+                    a.score_so_far
+                );
+                let score_b = get_current_score(
+                    &valves,
+                    &b.open_valves,
+                    b.score_so_far
+                );
+                score_a.cmp(&score_b)
+            });
+            current_gen = next_gen.into_iter().rev().take(50000).collect();
+            println!("Step {}", step + 1);
+        }
+
+        let score: u32 = current_gen.iter()
+            .map(|a| a.score_so_far)
+            .max()
+            .unwrap();
+
+            total_scores.push(score);
+            println!("{:?}", total_scores);
     }
 
-    let score: u32 = current_gen.iter()
-        .map(|a| a.score_so_far)
-        .max()
-        .unwrap();
-
-    println!("{}", score);
+    total_scores.sort();
+    println!("{:?}", total_scores);
 }
 
 fn get_current_score(
@@ -90,22 +130,31 @@ fn get_current_score(
     output
 }
 
-// fn get_possible_actions(
-//     valves: &HashMap<String, Valve>,
-//     open_valves: &HashSet<String>,
-//     positions: &[String; 2]
-// ) -> HashSet<String> {
-//     let mut actions: HashSet<String> = positions.iter()
-//         .map(|p| valves[p].connections)
-//         .flatten()
-//         .collect();
-//     // if !open_valves.contains(current_valve) {
-//     //     actions.push(current_valve.clone());
-//     // }
-//     actions.insert(positions[0]);
-//     actions.insert(positions[1]);
-//     actions
-// }
+fn dist(
+    valves: &HashMap<String, Valve>,
+    a: &String,
+    b: &String
+) -> u32 {
+    let mut scores: HashMap<&String, Option<u32>> = valves.keys()
+        .map(|a| (a, None))
+        .collect();
+    let mut queue = VecDeque::new();
+    queue.push_back(b);
+    scores.insert(b, Some(0));
+
+    while queue.len() > 0 {
+        let cur = queue.pop_front().unwrap();
+        let cur_score = scores[cur].unwrap();
+        for n in valves[cur].connections.iter() {
+            if let Some(score) = scores[n] {
+                if score <= cur_score + 1 { continue; }
+            }
+            scores.insert(n, Some(cur_score + 1));
+            queue.push_back(n);
+        }
+    }
+    scores[&a].unwrap()
+}
 
 fn get_possible_actions(
     valves: &HashMap<String, Valve>,
