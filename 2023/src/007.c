eeb73bd9d360c60ee16eaa5d3fede9bff0e97a09
maciekglowka <m.glowka@stabilis.io>
2023-12-07 15:47:26 +0100

    2023 7

--- 2023/src/007.c
+++ 2023/src/007.c
@@ -11,11 +11,15 @@
 #define MAX_ROWS 1000
 // this includes non existing 0 & 1 for easier indexing
 #define CARD_TYPES 15
+#define JOKER -1
 
 void first(FILE*);
-void get_row(char*, int*);
+void second(FILE*);
+void get_row(char*, int*, int);
 void get_counts(int*, int*);
+int get_counts_with_jokers(int*, int*);
 int score_hand(int*);
+int score_hand_with_jokers(int*, int);
 int cmp_hands(const void*, const void*);
 
 int main(int argc, char *argv[]) {
@@ -23,8 +27,8 @@ int main(int argc, char *argv[]) {
     textfile = fopen(argv[1], "r");
     if (textfile == NULL) return 1;
     first(textfile);
-    // rewind(textfile);
-    // second(textfile);
+    rewind(textfile);
+    second(textfile);
     return 0;
 }
 
@@ -34,7 +38,7 @@ void first(FILE* textfile) {
     int rows[MAX_ROWS][ROW_SIZE];
 
     while (fgets(line, LINE_LENGTH, textfile)) {
-        get_row(line, rows[row_count]);
+        get_row(line, rows[row_count], 11);
         int counts[5] = {0};
         get_counts(rows[row_count], counts);
         rows[row_count][SCORE_IDX] = score_hand(counts);
@@ -54,6 +58,32 @@ void first(FILE* textfile) {
     // }
 }
 
+void second(FILE* textfile) {
+    char line[LINE_LENGTH];
+    int row_count = 0;
+    int rows[MAX_ROWS][ROW_SIZE];
+
+    while (fgets(line, LINE_LENGTH, textfile)) {
+        get_row(line, rows[row_count], JOKER);
+        int counts[5] = {0};
+        int jokers = get_counts_with_jokers(rows[row_count], counts);
+        rows[row_count][SCORE_IDX] = score_hand_with_jokers(counts, jokers);
+        // printf("%d\n", score);
+        row_count++;
+    }
+
+    qsort(rows, row_count, 7 * sizeof(int), cmp_hands);
+    int sum = 0;
+    for (int i=0; i<row_count; i++) {
+        sum += (i + 1) * rows[i][BID_IDX];
+    }
+    printf("Second: %d\n", sum);
+    // for (int i=0; i<row_count; i++) {
+    //     for (int j=0; j<ROW_SIZE; j++) printf("%d ", rows[i][j]);
+    //     printf("\n");
+    // }
+}
+
 int cmp_hands(const void *a, const void *b) {
     if (((int*)a)[SCORE_IDX] > ((int*)b)[SCORE_IDX]) return 1;
     if (((int*)a)[SCORE_IDX] < ((int*)b)[SCORE_IDX]) return -1;
@@ -62,6 +92,34 @@ int cmp_hands(const void *a, const void *b) {
         if (((int*)a)[i] > ((int*)b)[i]) return 1;
         if (((int*)a)[i] < ((int*)b)[i]) return -1;
     }
+    return 0;
+}
+
+int score_hand_with_jokers(int *counts, int jokers) {
+    // hard coding ftw
+    if (counts[0] == 5) return 6;
+
+    if (counts[0] == 4 && jokers == 1) return 6;
+    if (counts[0] == 4) return 5;
+
+    if (counts[0] == 3 && jokers == 2) return 6;
+    if (counts[0] == 3 && jokers == 1) return 5;
+    if (counts[0] == 3 && counts[1] == 2) return 4;
+    if (counts[0] == 3) return 3;
+
+    if (counts[0] == 2 && jokers == 3) return 6;
+    if (counts[0] == 2 && jokers == 2) return 5;
+    if (counts[0] == 2 && jokers == 1 && counts[1] == 2) return 4;
+    if (counts[0] == 2 && jokers == 1) return 3;
+    if (counts[0] == 2 && counts[1] == 2) return 2;
+    if (counts[0] == 2) return 1;
+
+    if (jokers == 5) return 6;
+    if (jokers == 4) return 6;
+    if (jokers == 3) return 5;
+    if (jokers == 2) return 3;
+    if (jokers == 1) return 1;
+
     return 0;
 }
 
@@ -79,25 +137,40 @@ int cmp(const void *a, const void *b) {
     return *(int*)b - *(int*)a;
 }
 
+int get_counts_with_jokers(int *row, int *out) {
+    int counts[CARD_TYPES] = {0};
+    int jokers = 0;
+    for (int i=0; i<HAND_SIZE; i++) {
+        if (row[i] != JOKER) {
+            counts[row[i]]++;
+        } else {
+            jokers++;
+        }
+    }
+    qsort(counts, CARD_TYPES, sizeof(int), cmp);
+    // for (int i=0; i<CARD_TYPES; i++) printf("%d ", counts[i]);
+    // printf("\n");
+    for (int i=0; i<HAND_SIZE; i++) out[i] = counts[i];
+    return jokers;
+}
+
 void get_counts(int *row, int *out) {
     int counts[CARD_TYPES] = {0};
     for (int i=0; i<HAND_SIZE; i++) {
         counts[row[i]]++;
     }
     qsort(counts, CARD_TYPES, sizeof(int), cmp);
-    // for (int i=0; i<CARD_TYPES; i++) printf("%d ", counts[i]);
-    // printf("\n");
     for (int i=0; i<HAND_SIZE; i++) out[i] = counts[i];
 }
 
-void get_row(char *line, int *out) {
+void get_row(char *line, int *out, int j_value) {
     for (int i=0; i<5; i++) {
         char c = line[i];
         if (c >= 0x30 && c <= 0x39) {
             out[i] = c - 0x30;
         } else {
             if (c == 'T') out[i] = 10;
-            if (c == 'J') out[i] = 11;
+            if (c == 'J') out[i] = j_value;
             if (c == 'Q') out[i] = 12;
             if (c == 'K') out[i] = 13;
             if (c == 'A') out[i] = 14;
