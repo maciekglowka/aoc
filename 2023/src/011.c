a35270a49838e29623ff333ffe8f4e91f1254ecc
maciek.glowka <maciek.glowka@gmail.com>
2023-12-11 09:19:02 +0100

    2023 11 2wrong

--- 2023/src/011.c
+++ 2023/src/011.c
@@ -4,40 +4,41 @@
 #define LINE_LENGTH 255
 #define MAX_GALAXIES 512
 
-void first(FILE*);
+void solve(FILE*, long);
 // void second(FILE*);
 
 int main(int argc, char *argv[]) {
     FILE *textfile;
     textfile = fopen(argv[1], "r");
     if (textfile == NULL) return 1;
-    first(textfile);
-    // rewind(textfile);
+    solve(textfile, 1);
+    rewind(textfile);
+    solve(textfile, 10);
     // second(textfile);
     return 0;
 }
 
 int cmp_x(const void *a, const void *b) {
-    return ((int*)a)[0] - ((int*)b)[0];
+    return ((long*)a)[0] - ((long*)b)[0];
 }
 
-int manhattan(int ax, int ay, int bx, int by) {
+long manhattan(long ax, long ay, long bx, long by) {
     return abs(bx-ax) + abs(by-ay);
 }
 
-void first(FILE* textfile) {
+void solve(FILE* textfile, long offset) {
     char line[LINE_LENGTH];
     int galaxies_count = 0;
-    int galaxies[MAX_GALAXIES][2];
+    long galaxies[MAX_GALAXIES][2];
 
-    int row = 0;
+    long row = 0;
 
     while (fgets(line, LINE_LENGTH, textfile)) {
         int has_galaxy = 0;
-        for (int i=0; i<LINE_LENGTH; i++) {
+        for (long i=0; i<LINE_LENGTH; i++) {
             char c = line[i];
             if (c == '\n') {
-                if (!has_galaxy) row++;
+                if (!has_galaxy) row+=offset;
                 row++;
                 break;
             }
@@ -50,26 +51,26 @@ void first(FILE* textfile) {
         }
     }
 
-    qsort(galaxies, galaxies_count, 2 * sizeof(int), cmp_x);
+    qsort(galaxies, galaxies_count, 2 * sizeof(long), cmp_x);
 
-    int acc = 0;
-    int prev = 0;
+    long acc = 0;
+    long prev = 0;
     for (int i=0; i<galaxies_count; i++) {
-        int d = galaxies[i][0] - prev;
+        long d = galaxies[i][0] - prev;
         if (d>0) d--;
-        acc += d;
+        acc += d * offset;
         prev = galaxies[i][0];
         galaxies[i][0] += acc;
     }
 
-    long sum = 0;
+    long long sum = 0;
     for (int i=0; i<galaxies_count; i++) {
         for (int j=0; j<galaxies_count; j++) {
             if (i==j) continue;
-            sum += (long)manhattan(galaxies[i][0], galaxies[i][1], galaxies[j][0], galaxies[j][1]);
+            sum += (long long)manhattan(galaxies[i][0], galaxies[i][1], galaxies[j][0], galaxies[j][1]);
         }
     }
 
-    // for (int i=0; i<galaxies_count; i++) printf("G: %d %d; ", galaxies[i][0], galaxies[i][1]);
-    printf("First: %ld\n", sum / 2);
+    for (int i=0; i<galaxies_count; i++) printf("G: %ld %ld; ", galaxies[i][0], galaxies[i][1]);
+    printf("Result: %lld\n", sum / 2);
 }
