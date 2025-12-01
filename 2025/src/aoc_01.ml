a53c31540134ebcc55086b415f066eb1347a0ae3
maciekglowka <maciek.glowka@gmail.com>
2025-12-01 17:05:35 +0100

    2025 001

--- 2025/src/aoc_01.ml
+++ 2025/src/aoc_01.ml
@@ -1,24 +1,35 @@
-let read_lines file_name = In_channel.with_open_text file_name
-  In_channel.input_lines
+let read_lines file_name =
+  In_channel.with_open_text file_name In_channel.input_lines
 
-let str_lines = read_lines "001.txt"
+let str_lines = read_lines "../inputs/001.txt"
 
-let parse_line line = let a = String.split_on_char ' ' line
-  in int_of_string (List.hd a), int_of_string(List.nth a 3)
+let parse_line line =
+  let len = String.length line in
+  let sign = if line.[0] = 'L' then -1 else 1 in
+  sign * int_of_string (String.sub line 1 (len - 1))
 
-let a, b =
-  let pairs = List.map parse_line str_lines in
-  List.split pairs
+let input = List.map parse_line str_lines
 
-let sa, sb = 
-  List.sort compare a, List.sort compare b
+let solve cur step =
+  let n = (fst cur + step) mod 100 in
+  let n = if n < 0 then 100 + n else n in
+  let a = if n = 0 then snd cur + 1 else snd cur in
+  (n, a)
 
-let diff = List.map2 (fun a b -> abs(a - b)) sa sb
-let () = Printf.printf "First: %d\n" (List.fold_left (+) 0 diff)
+let a = List.fold_left solve (50, 0) input
+let () = Printf.printf "%d\n" (snd a)
 
-let sim =
-  let fi a = List.filter (fun b -> b = a) b in
-  let score a = a * (List.length (fi a)) in
-  List.map score a
+let solve2 cur step =
+  let v = fst cur + step in
+  let n = v mod 100 in
+  let a =
+    if step > 0 then v / 100
+    else if v > 0 then 0
+    else if fst cur > 0 then 1 + (v / -100)
+    else v / -100
+  in
+  let n = if n < 0 then 100 + n else n in
+  (n, snd cur + a)
 
-let () = Printf.printf "Second: %d\n" (List.fold_left (+) 0 sim)
+let a = List.fold_left solve2 (50, 0) input
+let () = Printf.printf "%d\n" (snd a)
